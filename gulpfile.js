8ab5bfe87cc59ae87f7b8fcbaccbcf6ff3dad957
Lukas Korten <lukas.korten@adesso.de>
2018-10-18 07:13:39 +0200

    add sass task and define it as default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
-var gulp = require('gulp');
 
-gulp.task('default', function () {
-    // place code for your default task here
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+
+gulp.task('sass', function () {
+    return gulp.src('./WebContent/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./WebContent/css'));
 });
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./WebContent/sass/**/*.scss', ['sass']);
+});
+
+
+gulp.task('default', ['sass']);
+gulp.task('build', ['sass']);
